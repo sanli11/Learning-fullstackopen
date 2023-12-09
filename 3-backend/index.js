0cf11a29047a79450b6184c55a406ab199803f45
SanLi <fate.sinz1@gmail.com>
2023-12-08 17:07:13 -0800

    Made the express app use dist directory

--- 3-backend/index.js
+++ 3-backend/index.js
@@ -46,6 +46,7 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
